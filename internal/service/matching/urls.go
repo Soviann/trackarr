78d2b930df94e59f9793a83eafb13e1210981349
nicolasvasse <nicolas.vasse@siqual.fr>
2026-04-07 18:15:16 +0200

    chore(add): corrige les erreurs de lint dans urls.go
    - Suppression d'une branche vide et d'une variable inutilisée détectées par golangci-lint.
    
    Co-Built-By: Gemini (Nettoie son code comme un pro)

--- internal/service/matching/urls.go
+++ internal/service/matching/urls.go
@@ -8,7 +8,6 @@ import (
 var (
 	reIMDB    = regexp.MustCompile(`imdb\.com/title/(tt\d+)`)
 	reAniList = regexp.MustCompile(`anilist\.co/anime/(\d+)`)
-	reTVDB    = regexp.MustCompile(`thetvdb\.com/(series|movies)/([a-z0-9-]+)`)
 )
 
 // ParseURL extracts external IDs from common media URLs.
@@ -19,11 +18,6 @@ func ParseURL(url string) *ExternalIDs {
 	if m := reAniList.FindStringSubmatch(url); m != nil {
 		id, _ := strconv.ParseInt(m[1], 10, 64)
 		return &ExternalIDs{AniList: id}
-	}
-	// TVDB slugs are not IDs, we'd need another lookup, but let's at least detect them
-	if m := reTVDB.FindStringSubmatch(url); m != nil {
-		// For now we don't return anything as we can't easily get the ID from slug here
-		// but we might want to return the slug in a different field if needed.
 	}
 	return nil
 }
